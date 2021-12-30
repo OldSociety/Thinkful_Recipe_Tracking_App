c63e07dba39a33324a8c1a6e4dcd69911ea01445
OldSociety <77307519+OldSociety@users.noreply.github.com>
2021-12-30 07:04:00 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import React, { useState } from 'react'
-import './App.css'
-import RecipeCreate from './RecipeCreate'
-import RecipeList from './RecipeList'
-import RecipeData from './RecipeData'
+import React, { useState } from "react";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData";
+import "./App.css";
 
 function App() {
-  const [recipes, setRecipes] = useState(RecipeData)
+ 
+  const [recipes, setRecipes] = useState(RecipeData);
 
-  // TODO: Add the ability for the <RecipeList /> component to list, edit and delete an existing recipe.
-  
+  const addRecipe = (newRecipe) => {
+    setRecipes([ ...recipes, newRecipe ]);
+  };
+
   function deleteRecipe(recipeIndex) {
     setRecipes(recipes.filter((recipe, index) => index !== recipeIndex));
   };
-  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
-
-  const handleCreate = (newRecipe) => setRecipes([...recipes, newRecipe])
-
-  
-  console.log(recipes)
 
   return (
     <div className="App">
-      <header>
-        <h1>Delicious Food Recipes</h1>
-      </header>
-      <RecipeList recipes={recipes} deleteRecipe ={deleteRecipe} />
-      <RecipeCreate handleCreate={handleCreate} />
+      <header><h1>Delicious Food Recipes</h1></header>
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe}/>
+      <RecipeCreate addRecipe={addRecipe} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
