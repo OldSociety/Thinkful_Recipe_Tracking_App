83f51ea4a7d3fd1e297aa5c7125b07239ccd053e
OldSociety <77307519+OldSociety@users.noreply.github.com>
2021-12-30 07:06:15 -0800

    Update RecipeTable.js

--- src/RecipeTable.js
+++ src/RecipeTable.js
@@ -1,29 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function RecipeTable({ recipes, deleteRecipe }) {
-  return recipes.map((recipe, index) => {
-    return (
-      <tr key={index}>
-        <td>{recipe.name}</td>
-        <td>{recipe.cuisine}</td>
-        <td>
-          <img src={recipe.photo} alt={`${recipe.photo}`}></img>
-        </td>
-        <td className="content_td">
-          <p>{recipe.ingredients}</p>
-        </td>
-        <td className="content_td">
-          <p>{recipe.preparation}</p>
-        </td>
-        <td>
-          <button>Edit</button>
-          <p />
-          <button name="delete" onClick={() => deleteRecipe(index)}>
-            Delete
-          </button>
-        </td>
-      </tr>
-    )
-  })
+function RecipeTable ({recipes, deleteRecipe}) {
+    //Return the recipes as a table.
+    return recipes.map((rec, index) => {
+        return (
+            <tr key={index}>
+                <td>{rec.name}</td>
+                <td>{rec.cuisine}</td>
+                <td><img src={rec.photo} alt={`Grahpicof ${rec.name}`} /></td>
+                <td className="content_td"><p>{rec.ingredients}</p></td>
+                <td className="content_td"><p>{rec.preparation}</p></td>
+                <td><button name="delete" onClick={() => deleteRecipe(index)}>Delete</button></td>
+            </tr>
+        );
+    });
 }
-export default RecipeTable
+
+export default RecipeTable;
