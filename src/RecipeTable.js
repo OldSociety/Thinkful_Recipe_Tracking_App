9906c72c12a090cc1eb99eb122130acdd148aa9b
OldSociety <77307519+OldSociety@users.noreply.github.com>
2021-12-30 07:06:28 -0800

    Update RecipeTable.js

--- src/RecipeTable.js
+++ src/RecipeTable.js
@@ -7,7 +7,7 @@ function RecipeTable ({recipes, deleteRecipe}) {
             <tr key={index}>
                 <td>{rec.name}</td>
                 <td>{rec.cuisine}</td>
-                <td><img src={rec.photo} alt={`Grahpicof ${rec.name}`} /></td>
+                <td><img src={rec.photo} alt={`Graphicof ${rec.name}`} /></td>
                 <td className="content_td"><p>{rec.ingredients}</p></td>
                 <td className="content_td"><p>{rec.preparation}</p></td>
                 <td><button name="delete" onClick={() => deleteRecipe(index)}>Delete</button></td>
